5c98ea3679740e2d13b61c611747e03c8ee24817
You-Hyuk <cxz6411@naver.com>
2025-11-17 03:35:22 +0900

    mod: 수강 바구니 전체 조회 DTO 수정

--- src/main/kotlin/com/example/course/domain/student/dto/LectureBasketDto.kt
+++ src/main/kotlin/com/example/course/domain/student/dto/LectureBasketDto.kt
@@ -4,12 +4,14 @@ import com.example.course.domain.student.entity.LectureBasket
 import com.example.course.domain.student.enums.Status
 
 data class LectureBasketDto(
+    val lectureBasketId: Long,
     val lectureBasketName: String,
     val status: Status
 ) {
     companion object {
         fun from(lectureBasket: LectureBasket): LectureBasketDto {
             return LectureBasketDto(
+                lectureBasketId = lectureBasket.id!!,
                 lectureBasketName = lectureBasket.name,
                 status = lectureBasket.status
             )
